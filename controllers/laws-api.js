8d222deda23b0dfa4b9aac7947825fefd1050f98
julien demarque <julien_demarque@hotmail.fr>
2019-01-10 02:46:20 +0200

    added api for page by page

---
+++ controllers/laws-api.js
@@ -0,0 +1,144 @@
+const Law = require("../models/law");
+const User = require("../models/user");
+
+exports.postNewLaw = function(req, res) {
+  //find the user
+  User.findOne({ email: req.user.email }, function(err, user) {
+    if (err) {
+      console.log(err);
+    } else {
+      //Create the law
+      const lawObj = {
+        title: req.body.lawTitle,
+        createdAt: new Date().getTime(),
+        description: req.body.lawDescription,
+        author: { id: user._id, username: user.username }
+      };
+      Law.create(lawObj, function(err, law) {
+        if (err) {
+          console.log(err);
+        } else {
+          law.save();
+          user.laws.push(law);
+          user.save();
+        }
+      });
+    }
+  });
+  res.send({ hi: "there Jul" });
+};
+
+exports.getRecentPerPage = function(req, res) {
+  const page = req.params.page;
+  const beginning = (page - 1) * 5;
+  Law.find({})
+    .populate("upVotes", "username", User)
+    .exec(function(err, allLaws) {
+      if (err) {
+        console.log(err);
+      } else {
+        const pageLaws = allLaws
+          .sort((a, b) => {
+            return b.createdAt - a.createdAt;
+          })
+          .slice(beginning, beginning + 5);
+
+        res.send(pageLaws);
+      }
+    });
+};
+
+exports.getTopPerPage = function(req, res) {
+  const page = req.params.page;
+  const beginning = (page - 1) * 5;
+  Law.find({})
+    .populate("upVotes", "username", User)
+    .exec(function(err, allLaws) {
+      if (err) {
+        console.log(err);
+      } else {
+        const pageLaws = allLaws
+          .sort((a, b) => {
+            return b.upVotes.length - a.upVotes.length;
+          })
+          .slice(beginning, beginning + 5);
+        res.send(pageLaws);
+      }
+    });
+};
+
+exports.getFiveRecent = function(req, res) {
+  Law.find({})
+    .populate("upVotes", "username", User)
+    .exec(function(err, allLaws) {
+      if (err) {
+        console.log(err);
+      } else {
+        const fiveMostRecentLaws = allLaws
+          .sort((a, b) => {
+            return b.createdAt - a.createdAt;
+          })
+          .slice(0, 5);
+        res.send(fiveMostRecentLaws);
+      }
+    });
+};
+
+exports.getTopFive = function(req, res) {
+  Law.find({})
+    .populate("upVotes", "username", User)
+    .exec(function(err, allLaws) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("ALL LAWS: ", allLaws);
+        const fiveTopLaws = allLaws
+          .sort((a, b) => {
+            console.log("a: ", a.title, a.upVotes.length);
+            console.log("b: ", b.title, b.upVotes.length);
+            return b.upVotes.length - a.upVotes.length;
+          })
+          .slice(0, 5);
+        console.log("FIVE TOP LAWS: ", fiveTopLaws);
+        res.send(fiveTopLaws);
+      }
+    });
+};
+
+exports.upvoteLaw = function(req, res) {
+  console.log("req.body", req.body);
+  console.log("req.user", req.user);
+
+  Law.findOne({ title: req.body.lawTitle })
+    .populate("upVotes", "username", User)
+    .exec(function(err, law) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("law line 70", law);
+        User.findOne({ username: req.user.username }, function(err, user) {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log("user line 75", user);
+            // check if the user is already in the array of the law
+            const userAlreadyVotedUp = law.upVotes.find(voters => {
+              return voters.username === user.username;
+            });
+
+            if (userAlreadyVotedUp) {
+              // if already in the list, remove it
+              law.upVotes = law.upVotes.filter(voters => {
+                return voters.username !== user.username;
+              });
+            } else {
+              // if not in the list, add the user to the list
+              law.upVotes.push(user);
+            }
+            law.save();
+            res.send(err || "saved your vote!");
+          }
+        });
+      }
+    });
+};
--- router.js
+++ router.js
@@ -1,6 +1,7 @@
-const Authentication = require("./controllers/authentication");
-const passportService = require("./services/passport");
 const passport = require("passport");
+const Authentication = require("./controllers/authentication");
+const LawAPI = require("./controllers/laws-api");
+const passportService = require("./services/passport");
 const Law = require("./models/law");
 const User = require("./models/user");
 
@@ -8,113 +9,26 @@ const requireAuth = passport.authenticate("jwt", { session: false });
 const requireSignin = passport.authenticate("local", { session: false });
 
 module.exports = function(app) {
+  //LAW API
   // Create a new LAW
-  app.post("/law", requireAuth, function(req, res) {
-    // Great we are getting the data from the form and the user info decoded
-    // Now we need to save the law in mongodb... :)
-    // console.log("req.body", req.body);
-    // console.log("req.user", req.user);
-    //find the user
-    User.findOne({ email: req.user.email }, function(err, user) {
-      if (err) {
-        console.log(err);
-      } else {
-        //Create the law
-        const lawObj = {
-          title: req.body.lawTitle,
-          createdAt: new Date().getTime(),
-          description: req.body.lawDescription,
-          author: { id: user._id, username: user.username }
-        };
-        Law.create(lawObj, function(err, law) {
-          if (err) {
-            console.log(err);
-          } else {
-            law.save();
-            user.laws.push(law);
-            user.save();
-          }
-        });
-      }
-    });
-    res.send({ hi: "there Jul" });
-  });
+  app.post("/law", requireAuth, LawAPI.postNewLaw);
 
   //Get recent LAWS
-  app.get("/recent", function(req, res) {
-    Law.find({})
-      .populate("upVotes", "username", User)
-      .exec(function(err, allLaws) {
-        if (err) {
-          console.log(err);
-        } else {
-          const fiveMostRecentLaws = allLaws
-            .sort((a, b) => {
-              return a.createdAt < b.createdAt;
-            })
-            .slice(0, 5);
-          res.send(fiveMostRecentLaws);
-        }
-      });
-  });
+  app.get("/recent/pages/:page", LawAPI.getRecentPerPage);
 
   //Get top LAWS
-  app.get("/top", function(req, res) {
-    Law.find({})
-      .populate("upVotes", "username", User)
-      .exec(function(err, allLaws) {
-        if (err) {
-          console.log(err);
-        } else {
-          const fiveTopLaws = allLaws
-            .sort((a, b) => {
-              return a.upVotes.length < b.upVotes.length;
-            })
-            .slice(0, 5);
-          res.send(fiveTopLaws);
-        }
-      });
-  });
+  app.get("/top/pages/:page", LawAPI.getTopPerPage);
+
+  //Get 5 recent LAWS
+  app.get("/recent", LawAPI.getFiveRecent);
+
+  //Get 5 top LAWS
+  app.get("/top", LawAPI.getTopFive);
 
   // Upvote a LAW
-  app.post("/upvote/:lawTitle", requireAuth, function(req, res) {
-    console.log("req.body", req.body);
-    console.log("req.user", req.user);
+  app.post("/upvote/:lawTitle", requireAuth, LawAPI.upvoteLaw);
 
-    Law.findOne({ title: req.body.lawTitle })
-      .populate("upVotes", "username", User)
-      .exec(function(err, law) {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log("law line 70", law);
-          User.findOne({ username: req.user.username }, function(err, user) {
-            if (err) {
-              console.log(err);
-            } else {
-              console.log("user line 75", user);
-              // check if the user is already in the array of the law
-              const userAlreadyVotedUp = law.upVotes.find(voters => {
-                return voters.username === user.username;
-              });
-
-              if (userAlreadyVotedUp) {
-                // if already in the list, remove it
-                law.upVotes = law.upVotes.filter(voters => {
-                  return voters.username !== user.username;
-                });
-              } else {
-                // if not in the list, add the user to the list
-                law.upVotes.push(user);
-              }
-              law.save();
-              res.send(err || "saved your vote!");
-            }
-          });
-        }
-      });
-  });
-
+  //AUTH
   app.post("/signin", requireSignin, Authentication.signin);
   app.post("/signup", Authentication.signup);
 };
